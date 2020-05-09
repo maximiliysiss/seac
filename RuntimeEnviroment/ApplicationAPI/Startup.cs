93a4e00a14218ced1956144396ddf2c55a378f1a
Maxim Zimin <zim.ma2010@yandex.ru>
2020-05-09 17:17:20 +0300

    application config

--- RuntimeEnviroment/ApplicationAPI/Startup.cs
+++ RuntimeEnviroment/ApplicationAPI/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CommonCoreLibrary.Startup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ApplicationAPI
 {
@@ -26,15 +20,22 @@ namespace ApplicationAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddJwtAuth(Configuration);
+            services.AddConsul(Configuration);
+            services.AddSwagger(Configuration["ApiInfo:Name"], Configuration["ApiInfo:Version"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime hostApplicationLifetime)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.RegisterWithConsul(hostApplicationLifetime);
+            app.UseSwaggerWithUI(Configuration["ApiInfo:Name"], Configuration["ApiInfo:Version"]);
 
             app.UseHttpsRedirection();
 
